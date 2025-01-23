3ae245086217d3cb15da877eca763d72fb3de090
giivict <giivictoria26@gmail.com>
2025-01-23 16:26:54 -0300

    adicionando a fonte da aplicação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
+
+import {
+  useFonts,
+  Karla_400Regular,
+  Karla_700Bold,
+} from "@expo-google-fonts/karla";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {fontsLoaded ? (
+        <Text style={{ fontFamily: "Karla_700Bold" }}>Home</Text>
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
