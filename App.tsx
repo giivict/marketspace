7b9959435513a732346cb10cda189bd6e5b182fd
giivict <giivictoria26@gmail.com>
2025-01-24 10:07:32 -0300

    ajustando o theme

--- App.tsx
+++ App.tsx
@@ -5,22 +5,26 @@ import {
   Karla_400Regular,
   Karla_700Bold,
 } from "@expo-google-fonts/karla";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <StatusBar
-        barStyle={"dark-content"}
-        backgroundColor={"transparent"}
-        translucent
-      />
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: "Karla_700Bold" }}>Home</Text>
-      ) : (
-        <View />
-      )}
-    </View>
+    <GluestackUIProvider config={config}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <StatusBar
+          barStyle={"dark-content"}
+          backgroundColor={"transparent"}
+          translucent
+        />
+        {fontsLoaded ? (
+          <Text style={{ fontFamily: "Karla_700Bold" }}>Home</Text>
+        ) : (
+          <View />
+        )}
+      </View>
+    </GluestackUIProvider>
   );
 }
