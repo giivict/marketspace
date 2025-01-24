47f368be0b98403984a0e673f70951e55ffb6ec9
giivict <giivictoria26@gmail.com>
2025-01-24 15:38:34 -0300

    Component Loading

--- App.tsx
+++ App.tsx
@@ -1,30 +1,31 @@
-import { StatusBar, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 
 import {
   useFonts,
   Karla_400Regular,
   Karla_700Bold,
 } from "@expo-google-fonts/karla";
-import { GluestackUIProvider } from "@gluestack-ui/themed";
-import { config } from "@gluestack-ui/config";
+import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { config } from "./config/gluestack-ui.config";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
 
   return (
     <GluestackUIProvider config={config}>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <StatusBar
-          barStyle={"dark-content"}
-          backgroundColor={"transparent"}
-          translucent
-        />
-        {fontsLoaded ? (
+      <StatusBar
+        barStyle={"dark-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      {!fontsLoaded ? (
+        <Center>
           <Text style={{ fontFamily: "Karla_700Bold" }}>Home</Text>
-        ) : (
-          <View />
-        )}
-      </View>
+        </Center>
+      ) : (
+        <Loading />
+      )}
     </GluestackUIProvider>
   );
 }
